aad19f6e17b078bf6a3b76c19935e696a8120308
Cyanne Jones <jonescyanne@yahoo.com>
2022-08-02 16:56:13 -0600

    Setup Router for use with the app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import { Route } from "react-router-dom"
+import { Route, NavLink, Routes } from "react-router-dom"
+import News from "../news/News"
+import Stats from "../stats/Stats"
+import ItemShop from "../item-shop/ItemShop"
 
 function App() {
+
+
   return (
     <div className="App">
-      hello world!
+      <header>
+        <h1>FORTBYTE</h1>
+        <nav>
+          <NavLink to="/">News</NavLink>
+          <NavLink to="stats">Stats</NavLink>
+          <NavLink to="shop">Item shop</NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<News />} />
+        <Route path="stats" element={<Stats />} />
+        <Route path="shop" element={<ItemShop />} />
+      </Routes>
     </div>
   );
 }
