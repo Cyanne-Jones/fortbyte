6559f0ef447301a913edee4baacdcffc2dbcc68a
Cyanne Jones <jonescyanne@yahoo.com>
2022-08-02 17:57:23 -0600

    Style NavLinks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,26 +1,39 @@
+import '../shared-styles.css'
 import './App.css';
 import { Route, NavLink, Routes } from "react-router-dom"
 import News from "../news/News"
 import Stats from "../stats/Stats"
 import ItemShop from "../item-shop/ItemShop"
+import { getValue } from '@testing-library/user-event/dist/utils';
 
 function App() {
-
 
   return (
     <div className="App">
       <header>
         <h1>FORTBYTE</h1>
         <nav>
-          <NavLink to="/">News</NavLink>
-          <NavLink to="stats">Stats</NavLink>
-          <NavLink to="shop">Item shop</NavLink>
+          <NavLink to="/" className={({isActive}) => 
+            (isActive ? "yellow-gradient blue-font" : "")}
+          >
+            News
+          </NavLink>
+          <NavLink to="stats" className={({isActive}) => 
+            (isActive ? "yellow-gradient blue-font" : "")}
+          >
+            Stats
+          </NavLink>
+          <NavLink to="shop" className={({isActive}) => 
+            (isActive ? "yellow-gradient blue-font" : "")}
+          >
+            Item shop
+          </NavLink>
         </nav>
       </header>
       <Routes>
-        <Route path="/" element={<News />} />
-        <Route path="stats" element={<Stats />} />
-        <Route path="shop" element={<ItemShop />} />
+        <Route exact path="/" element={<News />} />
+        <Route exact path="stats" element={<Stats />} />
+        <Route exact path="shop" element={<ItemShop />} />
       </Routes>
     </div>
   );
