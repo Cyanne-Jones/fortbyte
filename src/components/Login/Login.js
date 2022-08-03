607b7c8872fdda6e5494da4fcb97fbe1e4cf313f
Cyanne Jones <jonescyanne@yahoo.com>
2022-08-03 11:05:49 -0600

    Add error handling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,18 +7,27 @@ const Login = () => {
   const setUserStats = useDataStore((state) => state.setUserStats)
   const setUserName = useDataStore((state) => state.setUserName)
   const [userInput, setUserInput] = useState("");
+  const [error, setError] = useState("")
 
   const logUserIn = (event) => {
     event.preventDefault();
+    setError('')
     setUserName(userInput)
     fetch(`https://fortnite-api.com/v2/stats/br/v2?name=${userInput}`, {
       headers: {
         Authorization: process.env.REACT_APP_API_KEY
       }
     })
-    .then(res => res.json())
+    .then(res => {
+      if(res.status === 200) {
+        return res => res.json()
+      }
+      else if (res.status === 400) {
+        throw new Error("Invalid username, try again")
+      }
+    })
     .then(res => setUserStats(res))
-    .catch(error => console.log(error))
+    .catch(errorMsg => setError(errorMsg))
   }
 
   const enterUsername = (event) => {
@@ -28,6 +37,7 @@ const Login = () => {
   return (
     <div className="login yellow-gradient box-shadow">
       <h2 className="blue-gradient box-shadow">Enter an Epic Username</h2>
+      {error ? "Looks like you've entered an invalid username!" : ''}
       <form>
         <input
           type="text"
