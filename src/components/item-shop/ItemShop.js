f52877fe13539669b7cea18e80502984cc6a0283
Cyanne-Jones <jonescyanne@yahoo.com>
2024-07-01 22:16:31 -0600

    Clean up filter functionality a little and style

--- src/components/item-shop/ItemShop.js
+++ src/components/item-shop/ItemShop.js
@@ -6,42 +6,43 @@ import "../shared-styles.css"
 
 const ItemShop = () => {
 
-  const shopItems = useDataStore((state) => state.shopItems);
-  const [sortMode, setSortMode] = React.useState('showAll'); // bundles || outfit || pickaxes || emotes || wraps || gliders || contrails || backblings || music || loadingScreens ||
-
   const filters = [
     {
-      name: 'showAll',
-      items: shopItems,
-      displayName: 'Show All'
+      displayName: 'Show All',
+      filterValue: 'showAll'
     },
     {
-      name: 'bundles',
-      items: shopItems.filter(item => item.bundleName !== 'solo'),
-      displayName: 'Bundles'
-      
+      displayName: 'Bundles',
+      filterValue: 'bundles'
     },
     {
-      name: 'outfit',
-      items: shopItems.filter(item => item.type === 'Outfit'),
-      displayName: 'Outfits'
+      displayName: 'Outfits',
+      filterValue: 'Outfit'
     },
     {
-      name: 'pickaxes',
-      items: shopItems.filter(item => item.type === 'Pickaxe'),
-      displayName: 'Pickaxes'
+      displayName: 'Pickaxes',
+      filterValue: 'Pickaxe'
     },
     {
-      name: 'emotes',
-      items: shopItems.filter(item => item.type === 'Emote'),
-      displayName: 'Emotes'
+      displayName: 'Emotes',
+      filterValue: 'Emote'
     },
     {
-      name: 'backBling',
-      items: shopItems.filter(item => item.type === 'Back Bling'),
-      displayName: 'Back Bling'
+      displayName: 'Back Bling',
+      filterValue: 'Back Bling'
     },
+    {
+      displayName: 'Wraps',
+      filterValue: 'Wrap'
+    },
+    {
+      displayName: 'Glider',
+      filterValue: 'Glider'
+    }
   ]
+
+  const shopItems = useDataStore((state) => state.shopItems);
+  const [sortMode, setSortMode] = React.useState('showAll');
 
   const sortedShopItems = shopItems.reduce((acc, item) => {
 
@@ -61,70 +62,38 @@ const ItemShop = () => {
   }, {bundles: [], soloItems: []})
 
 
-  const unbundledShopItems = sortedShopItems.soloItems.map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} />);
-  
-  const allShopItems = shopItems.map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} />);
-
-  const bundledShopItems = sortedShopItems.bundles.map((bundle, index) => {
-    const mappedItems = bundle.items.map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} />);
+  const bundledShopItems = sortedShopItems.bundles.map((bundle) => {
+    const shopThings = bundle.items.map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} />);
     return (
       <div className="bundle-container" key={bundle.name}>
         <div key={bundle.name} className="bundle">
           <h2 className="bundle-name">{bundle.name}</h2>
           <div className="bundle-items">
-            {mappedItems}
+            {shopThings}
           </div>
         </div>
       </div>
     )
   });
 
-  const outfits = shopItems.filter(item => item.type === 'Outfit').map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} />);
-  const pickaxes = shopItems.filter(item => item.type === 'Pickaxe').map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} />);
-  const emotes = shopItems.filter(item => item.type === 'Emote').map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} /> );
-  const backBling = shopItems.filter(item => item.type === 'Back Bling').map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} />);
-
-  
+  const shopItemMapper = (type) =>  {
+    if (type === 'showAll') return shopItems.map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} />);
+    if (type === 'bundles') return bundledShopItems;
+    const filteredItems = shopItems.filter(item => item.type === type);
+    return filteredItems.map((item, index) => <ShopThing key={`${item.id}${index}`} item={item} />);
+  }
 
   return (
     <div className="shop">
       <h2 className="shop-update">Items updated daily at 0:00UTC</h2>
+      <div className='filter-button-group'>
       {filters.map(filter =>(
-        <button key={filter.name} onClick={() => setSortMode(filter.name)} style={{backgroundColor: sortMode === filter.name ? 'cyan' : 'yellow'}}>{filter.displayName}</button>
+        <button className="filter-button" key={filter.filterValue} onClick={() => setSortMode(filter.filterValue)} style={{backgroundColor: sortMode === filter.filterValue ? 'cyan' : 'yellow'}}>{filter.displayName}</button>
       ))}
+      </div>
 
       <div className="item-container">
-        {sortMode === 'bundles' && (
-          <>
-          {bundledShopItems}
-          {/* {unbundledShopItems} */}
-        </>
-        )}
-        {sortMode === 'outfit' && (
-          <>
-            {outfits}
-          </>
-        )}
-        {sortMode === 'pickaxes' && (
-          <>
-            {pickaxes}
-          </>
-        )}
-        {sortMode === 'showAll' && (
-          <>
-            {allShopItems}
-          </>
-        )}
-        {sortMode === 'emotes' && (
-          <>
-            {emotes}
-          </>
-        )}
-        {sortMode === 'backBling' && (
-          <>
-            {backBling}
-          </>
-        )}
+        {shopItemMapper(filters.find(filter => filter.filterValue === sortMode).filterValue)}
       </div>
     </div>
   )
